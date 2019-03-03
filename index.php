4e49640ea9cf8afa8229eb0370e25e6877eee2ae
Engels Hernandez <enyiyo@gmail.com>
2019-03-02 21:38:56 -0400

    modified index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <body>
         <h1>Título aqu&iacute;</h1>
         <p>Algo aqu&iacute;</p>
-        <?php echo '<p>Hello, World!</p>'; ?>
+        <?php echo '<p>Hello, World!</p>'; ?> 
+        <?php echo phpinfo(); ?>
     </body>
 </html>
