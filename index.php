eb3fdd928142b1a253b6fd542e52d765c9bc51e2
Engels Hernandez <enyiyo@gmail.com>
2019-03-03 01:39:20 -0400

    modified index.php

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
         <h1>Título aqu&iacute;</h1>
         <p>Algo aqu&iacute;</p>
         <?php echo '<p>Hello, World!</p>'; ?> 
-        <?php echo phpinfo(); ?>
+        <?php echo $_SERVER['HTTP_USER_AGENT']; ?>
     </body>
 </html>
